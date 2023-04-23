6d09a5f26cae4c759de5cb98ffd5c9982bbc6e9a
Sergii <kovalshmoval@gmail.com>
2023-04-23 16:19:49 +0200

    add first files

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+@app.route('/weather')
+def weather():
+    return render_template('weather.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
